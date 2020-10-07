a95d2423677a55c8235d8410deb114b2f1e08446
Søren Vedel <sorens.stuff@hotmail.com>
2020-10-07 20:30:18 +0200

    changed styling of local constant variable

--- tests/unit/custom_models/test_simple_matern_model.py
+++ tests/unit/custom_models/test_simple_matern_model.py
@@ -17,15 +17,15 @@ def test_SimpleCustomMaternGP_definitions(custom_models_simple_training_data):
     # define the model
     model = SimpleCustomMaternGP(train_X, train_Y, nu=None)
 
-    zero_tol = 1e-6
+    ZERO_TOL = 1e-6
 
     # assert non-trained model defined with constant mean of 0
     assert "mean_module.constant" in model.state_dict()
-    assert abs(model.state_dict()["mean_module.constant"].item()) < zero_tol
+    assert abs(model.state_dict()["mean_module.constant"].item()) < ZERO_TOL
 
     # assert covars module
-    assert abs(model.state_dict()["covar_module.raw_outputscale"].item()) < zero_tol
-    assert abs(model.state_dict()["covar_module.base_kernel.raw_lengthscale"].item()) < zero_tol
+    assert abs(model.state_dict()["covar_module.raw_outputscale"].item()) < ZERO_TOL
+    assert abs(model.state_dict()["covar_module.base_kernel.raw_lengthscale"].item()) < ZERO_TOL
 
 
 @pytest.mark.parametrize("nu", [None, 2.5, 1.5])
@@ -39,7 +39,7 @@ def test_SimpleCustomMaternGP_train(custom_models_simple_training_data, nu):
     train_X = custom_models_simple_training_data[0]
     train_Y = custom_models_simple_training_data[1]
 
-    zero_tol = 1e-6
+    ZERO_TOL = 1e-6
 
     # define the model
     model = SimpleCustomMaternGP(train_X, train_Y, nu=nu)
@@ -55,10 +55,10 @@ def test_SimpleCustomMaternGP_train(custom_models_simple_training_data, nu):
     fit_gpytorch_model(ll)
 
     # assert that mean has been updated
-    assert abs(model.state_dict()["mean_module.constant"].item() - train_Y.item()) < zero_tol
+    assert abs(model.state_dict()["mean_module.constant"].item() - train_Y.item()) < ZERO_TOL
 
     # assert that covariance has been updated
-    assert abs(model.state_dict()["covar_module.raw_outputscale"].item() - -26.510910603516837) < zero_tol
+    assert abs(model.state_dict()["covar_module.raw_outputscale"].item() - -26.510910603516837) < ZERO_TOL
 
 
 @pytest.mark.parametrize(
@@ -79,7 +79,7 @@ def test_SimpleCustomMaternGP_train_4elements(custom_models_simple_training_data
     train_X = custom_models_simple_training_data_4elements[0]
     train_Y = custom_models_simple_training_data_4elements[1]
 
-    zero_tol = 1e-6
+    ZERO_TOL = 1e-6
 
     # define the model
     model = SimpleCustomMaternGP(train_X, train_Y, nu=nu)
@@ -95,8 +95,8 @@ def test_SimpleCustomMaternGP_train_4elements(custom_models_simple_training_data
     fit_gpytorch_model(ll)
 
     # assert that mean has been updated
-    assert abs(model.state_dict()["mean_module.constant"].item() - mean_const) < zero_tol
+    assert abs(model.state_dict()["mean_module.constant"].item() - mean_const) < ZERO_TOL
 
     # assert that covariance has been updated
-    assert abs(model.state_dict()["covar_module.raw_outputscale"].item() - covar_raw_outputscale) < zero_tol
-    assert abs(model.state_dict()["covar_module.base_kernel.raw_lengthscale"].item() - covar_raw_lenghtscale) < zero_tol
+    assert abs(model.state_dict()["covar_module.raw_outputscale"].item() - covar_raw_outputscale) < ZERO_TOL
+    assert abs(model.state_dict()["covar_module.base_kernel.raw_lengthscale"].item() - covar_raw_lenghtscale) < ZERO_TOL
