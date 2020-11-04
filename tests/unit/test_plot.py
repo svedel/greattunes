725b43c96a2ed7e2a66f168a431582ac331d162a
Søren Vedel <sorens.stuff@hotmail.com>
2020-11-04 21:42:36 +0100

    added unit test for preditive_results

--- tests/unit/test_plot.py
+++ tests/unit/test_plot.py
@@ -116,3 +116,51 @@ def test_plot_convergence(custom_models_simple_training_data_4elements):
         assert round(ax.lines[0].get_ydata()[it],4) == round(y_axes_res[it], 4)
 
 
+def test_predictive_results_unit(ref_model_and_training_data):
+    """
+    test predictive_results for passing tests, using univariate data
+    """
+
+    # input from fixture
+    train_X = ref_model_and_training_data[0]
+    train_Y = ref_model_and_training_data[1]
+    model_obj = ref_model_and_training_data[2]
+    lh = ref_model_and_training_data[3]
+    ll = ref_model_and_training_data[4]
+
+    # define test class
+    class TmpClass:
+        def __init__(self):
+            self.train_X = train_X
+            self.train_Y = train_Y
+            self.model = {
+                "model_type": "SingleTaskGP",
+                "model": model_obj,
+                "likelihood": lh,
+                "loglikelihood": ll,
+            }
+
+        from creative_project._plot import predictive_results
+
+    # initialize class
+    cls = TmpClass()
+
+    # data at which to retrieve results
+    pred_X = torch.linspace(-1.5, 1.5, dtype=torch.double)
+
+    # execute method
+    mean_result, lower_bound, upper_bound = cls.predictive_results(pred_X=pred_X)
+
+    error_lim = 1e-6
+
+    # test mean_result
+    assert abs(round(mean_result[10].item(), 6) - 0.101979) < error_lim
+    assert abs(round(mean_result[75].item(), 6) - 0.490944) < error_lim
+
+    # test lower bound
+    assert abs(round(lower_bound[20].item(), 6) - -2.94204) < error_lim
+    assert abs(round(lower_bound[66].item(), 6) - -2.828493) < error_lim
+
+    # test upper bound
+    assert abs(round(upper_bound[17].item(), 6) - 3.215015) < error_lim
+    assert abs(round(upper_bound[95].item(), 6) - 3.626709) < error_lim
