9fe19388ed75a668d5476e42fdbcdc0c0c53ee18
Søren Vedel <sorens.stuff@hotmail.com>
2020-12-21 07:18:55 +0100

    added three methods to read and verify manually provided covariates before adding them to the aks-tell dynamics

--- creative_project/_observe.py
+++ creative_project/_observe.py
@@ -114,3 +114,91 @@ def _print_candidate_to_prompt(self, candidate):
     )
 
     return input_request
+
+
+def _covars_datapoint_observation(self): # NOT TESTED!!!
+    """
+    gets observation of actual covars x. Updates stored data, counters etc
+    assumes:
+        - covars x can only be one iteration ahead of observation y
+        - tracking of response observations is managed elsewhere, where it's also ensured that these are assigned to right counter
+    """
+
+    # iteration counter of proposed datapoint
+    obs_counter = self.model["covars_proposed_iter"]
+
+    # get and verify covars datapoint
+    covars_datapoint = self._get_and_verify_covars_input()
+
+    # store data
+    # first datapoint
+    if self.train_X is None:
+        self.train_X = covars_datapoint
+
+    # case where sampling this iteration for the second time, overwriting first sampled datapoint
+    elif self.train_X.shape[0] >= obs_counter:
+        self.train_X[obs_counter, :] = covars_datapoint
+    else:
+        self.train_X = torch.cat((self.train_X, covars_datapoint), dim=0)
+
+    # update counter
+    self.model["covars_sampled_iter"] = obs_counter
+
+
+def _get_and_verify_covars_input(self):
+    """
+    read and verify covars. Currently focused on manual input only, but to be expanded to allow multiple
+    different mechanism (if needed). Allowing up to 'MAX_ITER' repeats of providing the data and updates text to
+    prompt to indicate if candidates datapoint not accepted
+    """
+
+    MAX_ITER = 3
+    it = 0
+    accepted = False
+
+    additional_text = ""
+
+    while not accepted and it < MAX_ITER:
+
+        it += 1
+
+        # read covars
+        covars_candidate_float_tensor = self._read_covars_manual_input(additional_text)
+
+        # verify number of provided elements is correct
+        if self._Validators__validate_num_covars(covars_candidate_float_tensor):
+            accepted = True
+        else:
+            additional_text = " (REPEAT, prior attempt had incorrect number of datapoints) "
+
+    if accepted:
+        return covars_candidate_float_tensor
+    else:
+        add_text = ""
+        if self.proposed_X is not None:
+            add_text = " Was expecting something like " + str(self.proposed_X[-1]) + ", but got " + str(covars_candidate_float_tensor)
+        raise Exception("creative_project._observe._get_and_verify_covars_input: unable to get acceptable covariate input in " + str(MAX_ITER) + " iterations." + add_text)
+
+
+def _read_covars_manual_input(self, additional_text):
+    """
+    read covars as manual input from prompt
+    :param additional_text (str): additional text to be displayed in prompt
+    :return covars_candidate_float_tensor (torch.tensor): user-provided covariates
+    TODO:
+        - make more user friendly in terms of making sure user can make mistakes
+            - don't have to remember order of variables
+            - can see whether a variable has already been provided
+            - will need to accept at the end
+    """
+
+    assert isinstance(additional_text, str), "creative_project._observe._read_covars_manual_input: wrong datatype of parameter 'additional_text'. Was expecting 'str' but received " + str(type(additional_text))
+
+    # read candidate
+    covars_candidate_string = input("ITERATION " + str(self.model[
+                                                           "covars_proposed_iter"]) + additional_text + " - Please provide observed covariates separated by commas (" + str(
+        self.initial_guess.shape[0]) + " covariates): ")
+    covars_candidate_float_tensor = torch.tensor([[float(z) for z in covars_candidate_string.split(',')]],
+                                                 device=self.device, dtype=self.dtype)
+
+    return covars_candidate_float_tensor
