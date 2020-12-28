9b17fd8393d34d3f5991fa3ad45d57c49f65d79c
Søren Vedel <sorens.stuff@hotmail.com>
2020-12-28 08:16:35 +0100

    fixing basic naming error resulting form copying code snippet

--- tests/integration/test_observe_functional.py
+++ tests/integration/test_observe_functional.py
@@ -205,7 +205,7 @@ def test_covars_datapoint_observation_int_fails(tmp_observe_class, covariate_str
     """
 
     # special case for python version 3.7 (handled via new keyword argument to pytest)
-    if pythontestvers == "3.7" and response_str != "a":
+    if pythontestvers == "3.7" and covariate_str != "1, a, 2, 3":
         # removes the '' from the error message
         error_msg = error_msg[:-2]
 
