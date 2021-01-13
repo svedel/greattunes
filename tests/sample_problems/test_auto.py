d4d6080d9e18a432f3fdee750da1eba56d50c866
Søren Vedel <sorens.stuff@hotmail.com>
2021-01-13 05:24:00 +0100

    added new sample problems with 'rel_tol' as stopping criteria

--- tests/sample_problems/test_auto.py
+++ tests/sample_problems/test_auto.py
@@ -1,5 +1,6 @@
 import torch
 import pytest
+import numpy as np
 from creative_project import CreativeProject
 
 
@@ -47,6 +48,85 @@ def test_sample_problems_auto_1d_maximization(max_iter, max_response, error_lim,
     assert cc.best["iteration_when_recorded"] == max_iter
 
 
+# 1d maximization problem with rel_tol stopping iteration
+@pytest.mark.parametrize(
+    "max_iter, rel_tol, rel_tol_steps, num_iterations_exp, response_error_lim, covar_error_lim, model_type",
+    [
+        [50, 1e-12, None, 2, 1.5, 0.5, "SingleTaskGP"],  # test that iteration stops if relative improvement in one step is below rel_tol
+        [50, 1e-12, None, 2, 1.5, 0.5, "Custom"],  # test that iteration stops if relative improvement in one step is below rel_tol
+        [50, 1e-10, 5, 8, 2e-1, 0.07, "SingleTaskGP"],  # same as second case above but with realistic rel_tol
+        [50, 1e-10, 5, 12, 9e-2, 0.04, "Custom"],  # same as second case above but with realistic rel_tol
+    ]
+)
+def test_sample_problems_auto_1d_maximization_rel_tol_test(max_iter, rel_tol, rel_tol_steps, num_iterations_exp,
+                                                           response_error_lim, covar_error_lim, model_type):
+    """
+    test that 'rel_tol' and 'rel_tol_steps' stops the iteration before max_iter. Test both with rel_tol alone
+    (rel_tol_steps = None) as well as with both applied. Using a 1d problem with a known maximum value, the method
+    is tested for more than one model type.
+
+    For all cases tests that the number of required iterations is as specified by 'num_iterations_exp'. The goal is
+    twofold: 1) that the number of iterations is below max_iter, and 2) that it is the same as 'num_iterations_exp'.
+
+    NOTE: for the first two tests (those with 'rel_tol_steps' = None), the error on the found maximum response is big
+    (response_error_lim have been set above 1 to satisfy). We need to continuously monitor this poor convergence
+    """
+
+    # define data
+    x_input = [(0.5, 0, 1)]
+
+    # define response function
+    def f(x):
+        return -(6 * x - 2) ** 2 * torch.sin(12 * x - 4)
+
+    # initialize class instance
+    cc = CreativeProject(covars=x_input, model=model_type)
+
+    # run the auto-method
+    cc.auto(response_samp_func=f, max_iter=max_iter, rel_tol=rel_tol, rel_tol_steps=rel_tol_steps)
+
+    # assert that fewer iterations required than max_iter
+    assert cc.best_response_value.size()[0] == num_iterations_exp
+
+    # define local function to calculate relative improvement
+    def cal_rel_improvements(best_response_tensor, rel_tol, rel_tol_steps):
+
+        # special case where rel_tol_steps = None is dealt with by setting rel_tol_steps = 1 (since in this case only
+        # look back one step)
+        if rel_tol_steps is None:
+            rel_tol_steps = 1
+
+        all_below_rel_tol = False
+
+        # first build tensor with the last rel_tol_steps entries in self.best_response_value and the last
+        # rel_tol_steps+1 entries
+        tmp_array = torch.cat(
+            (best_response_tensor[-(rel_tol_steps + 1):-1], best_response_tensor[-(rel_tol_steps):]),
+            dim=1).numpy()
+
+        # calculate the relative differences
+        tmp_rel_diff = np.diff(tmp_array, axis=1) / best_response_tensor[-(rel_tol_steps):].numpy()
+
+        # determine if all below 'rel_tol'
+        below_rel_tol = [rel_dif[0] < rel_tol for rel_dif in tmp_rel_diff.tolist()]
+
+        # only accept if the relative difference is below 'rel_tol' for all steps
+        if sum(below_rel_tol) == rel_tol_steps:
+            all_below_rel_tol = True
+
+        return all_below_rel_tol
+
+    # assert that relative improvement is below rel_tol for rel_tol_steps number of steps. For special case of
+    # rel_tol_steps = None only investigate last step
+    assert cal_rel_improvements(cc.best_response_value, rel_tol, rel_tol_steps)
+
+    # assert that max value found (both the covariate and the response)
+    THEORETICAL_MAX_COVAR = 0.75725
+    MAX_RESPONSE = 6.02073
+    assert abs(cc.covars_best_response_value[-1].item() - THEORETICAL_MAX_COVAR) / THEORETICAL_MAX_COVAR  < covar_error_lim
+    assert abs(cc.best_response_value[-1].item() - MAX_RESPONSE) / MAX_RESPONSE < response_error_lim
+
+
 # add new test with multivariate covariates
 @pytest.mark.parametrize(
     "max_iter, error_lim, x0_0, x1_0",
@@ -61,8 +141,6 @@ def test_sample_problems_auto_2d_maximization(max_iter, error_lim, x0_0, x1_0):
     solves a 2D maximzation problem for the negative Easom standard function for optimization. This function has a
     narrow peak at (x_0,x_1) = (0,0), otherwise is flat 0 (see details here https://www.sfu.ca/~ssurjano/easom.html)
     """
-
-    #max_iter = 50
 
     # define the function (negative of the Easom function)
     def f2(x):
