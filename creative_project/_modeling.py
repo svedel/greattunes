c3d8c550b92bee958f6f06bdcbe19734cadc3dbe
Søren Vedel <sorens.stuff@hotmail.com>
2021-04-05 17:12:35 +0200

    try commenting out state_dict stuff

--- creative_project/_modeling.py
+++ creative_project/_modeling.py
@@ -1,6 +1,7 @@
 from botorch.fit import fit_gpytorch_model
 from gpytorch.likelihoods import GaussianLikelihood
 from gpytorch.mlls import ExactMarginalLogLikelihood
+from torch.optim import SGD
 from creative_project.custom_models.simple_matern_model import SimpleCustomMaternGP
 from creative_project.transformed_kernel_models.GPregression import (
     SingleTaskGP_transformed,
@@ -23,8 +24,6 @@ def _set_GP_model(self, **kwargs):
         - self.model["loglikelihood"] (BoTorch log-likelihood):
     :return model_retrain_succes_str (str)
     """
-
-    # TODO: check that self.model['model_type'] value is allowed
 
     # FixedNoiseGP is a BoTorch alternative that also includes a fixed noise estimate on the observations train_Y
     if self.model["model_type"] == "SingleTaskGP":
@@ -78,11 +77,18 @@ def _set_GP_model(self, **kwargs):
                 # overwrite entries in the existing state dict
                 model_dict.update(pretrained_dict)
 
+                # Load parameters without standard shape checking.
+                #model_obj.load_strict_shapes(False)
+
                 # load the new state dict
-                model_obj.load_state_dict(pretrained_dict)
+                #model_obj.load_state_dict(pretrained_dict)
+
+                #if self.model["model_type"] == "Custom":
+                #    print(pretrained_dict)
 
     # fit the underlying model
     fit_gpytorch_model(ll)
+    #model_obj = _custom_model_fitter(model_obj, ll, self.train_X, epochs=50, lr=0.1)
 
     # return model + likelihood
     self.model["model"] = model_obj
@@ -90,3 +96,24 @@ def _set_GP_model(self, **kwargs):
     self.model["loglikelihood"] = ll
 
     return "Successfully trained GP model"
+
+# def _custom_model_fitter(model_obj, ll, train_X, **kwargs):
+#     """
+#     custom model fitting routine using stochastic gradient descend
+#     :param self:
+#     :param train_X:
+#     :param ll:
+#     :return:
+#     """
+#
+#     model_obj.train()
+#
+#     optimizer = SGD([{'params': model_obj.parameters()}], lr=kwargs.get("lr"))
+#     for epoch in range(kwargs.get("epochs")):
+#         optimizer.zero_grad()
+#         output = model_obj(train_X)
+#         loss = -ll(output, model_obj.train_targets)
+#         loss.backward()
+#         optimizer.step()
+#
+#     return model_obj
